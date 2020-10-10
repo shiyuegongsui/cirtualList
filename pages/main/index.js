5a008a54ad085d9161e8c93380df830b42934769
shiyuegongsui <simply_love12@163.com>
2020-10-10 18:06:51 +0800

    细节优化

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,7 +1,9 @@
 // pages/main/index.js
-let baseList=[];
+let baseList = [];
+let timer = null;
 Page({
   data: {
+    minHeight:0,
     headerHeight: 0,
     footerHeight: 0,
     list: [],
@@ -13,7 +15,7 @@ Page({
       type: 0,//0：全部 1：查看关注人 2：查看自己
       pageSize: 20,
       startTime: ''
-  },
+    },
   },
 
   onLoad() {
@@ -39,75 +41,95 @@ Page({
     let nowList = [];
 
     wx.$ajax.post("/tool/b/album/get/dynamic", query).then(res => {
-      
-        if (query.startTime == '') {
-            baseList = [];
-        }
-        let { result: { list: resultList } } = res;
-        nowList = resultList;
-        baseList.push({
-            list: nowList
-        })
-        this.calcList();
-        // 修改baseList
-        if (resultList.length == 0 || (res.result.pageIndex == res.result.totalPages)) {
-            console.log("列表结束")
-        } else {
-            let lastItem = resultList[resultList.length - 1];
-            this.setData({
-                'query.startTime': lastItem.releaseT,
-                'query.id': lastItem.id
-            });
-        }
+
+      if (query.startTime == '') {
+        baseList = [];
+      }
+      let { result: { list: resultList } } = res;
+      nowList = resultList;
+      baseList.push({
+        list: nowList
+      })
+      this.calcList();
+      // 修改baseList
+      if (resultList.length == 0 || (res.result.pageIndex == res.result.totalPages)) {
+        console.log("列表结束")
+      } else {
+        let lastItem = resultList[resultList.length - 1];
+        this.setData({
+          'query.startTime': lastItem.releaseT,
+          'query.id': lastItem.id
+        });
+      }
     })
 
   },
   //重新计算list和头尾高度
   calcList() {
-    
-    let that = this;
-    let { index, heightArr } = this.data;
-    let headerHeight = 0;
-    let footerHeight = 0;
-    let list = [];
-    baseList.forEach((e, i) => {
-      if (i < (index - 1) && index >= 1) {
-        heightArr[i] && (headerHeight += heightArr[i].height);
-      } else if (i > (index + 1) && (index + 1) <= baseList.length) {
-        heightArr[i] && (footerHeight += heightArr[i].height);
-      } else {
-        e.index = i;
-        list.push(e);
-      }
-    })
-    this.setData({
-      headerHeight,
-      footerHeight,
-      list
-    }, () => {
-      //计算高度
-      const query = this.createSelectorQuery()
-      query.selectAll('.cirtual-list').boundingClientRect()
-      query.selectViewport().scrollOffset()
-      query.exec(function (res) {
-        res[0].forEach((e) => {
-          let { index } = e.dataset;
-          let begin = heightArr[index - 1] ? heightArr[index - 1].begin + e.height : 0
-          heightArr[index] = {
-            height: e.height,
-            begin,
-            end: begin + e.height
-          }
-        })
-        that.setData({
-          heightArr
+
+    timer && clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      let that = this;
+      let { index, heightArr } = this.data;
+      let headerHeight = 0;
+      let footerHeight = 0;
+      let list = [];
+      baseList.forEach((e, i) => {
+        if (i < (index - 1) && index >= 1) {
+          heightArr[i] && (headerHeight += heightArr[i].height);
+        } else if (i > (index + 1) && (index + 1) <= baseList.length) {
+          heightArr[i] && (footerHeight += heightArr[i].height);
+        } else {
+          e.index = i;
+          list.push(e);
+        }
+      })
+      console.log(heightArr);
+      this.setData({
+        headerHeight,
+        footerHeight
+      })
+      this.setData({
+        list
+      }, () => {
+        //计算高度
+        const query = this.createSelectorQuery()
+        query.selectAll('.cirtual-list').boundingClientRect()
+        query.selectViewport().scrollOffset()
+        query.exec(function (res) {
+          res[0].forEach((e) => {
+            let { index } = e.dataset;
+            let begin = heightArr[index - 1] ? heightArr[index - 1].begin + heightArr[index - 1].height : 0
+            if (index == undefined) {
+              return;
+            }
+            heightArr[index] = {
+              height: e.height,
+              begin,
+              end: begin + e.height
+            }
+          })
+
+          let minHeight= heightArr.reduce((value,e)=>{
+            return value+=e.height
+          },0)
+          that.setData({
+            heightArr,
+            minHeight
+          })
         })
       })
-    })
+
+    }, 400);
   },
 
   onPageScroll(e) {
+
+
     let scrollTop = e.scrollTop;
+
+    console.log(scrollTop);
     let index = this.data.heightArr.findIndex((e) => {
       return scrollTop >= e.begin && scrollTop < e.end
     });
